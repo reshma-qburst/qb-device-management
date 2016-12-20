6c89f54157cc93e62e297ea162a2d52e7f4ac4b2
reshma <reshma@qburst.com>
2016-12-20 18:12:22 +0530

    Project Setup - Configure Webpack

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+require('core-js');
+require('reflect-metadata');
+require('zone.js/dist/zone');
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app/app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
