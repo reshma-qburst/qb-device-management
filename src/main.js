bb05a9ffaa9863d94abd6f4694d14cec48443ce3
reshma <reshma@qburst.com>
2016-12-21 11:31:34 +0530

    initial folder structure

--- src/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require('core-js');
-require('reflect-metadata');
-require('zone.js/dist/zone');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./app/app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
