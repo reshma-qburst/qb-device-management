8b12c8e8cd4d952128c6ad99942b8fbc4d9851cd
reshma <reshma@qburst.com>
2016-12-22 10:27:45 +0530

    adding comments to files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My App with webpack new</h1>'
+	template: '<h1>Hello {{name}} !!!</h1>'
 })
 
-export class AppComponent { }
+export class AppComponent {
+	name = "John";
+ }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'core-js';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
 
-bootstrap(AppComponent);
+platformBrowserDynamic().bootstrapModule(AppModule);
