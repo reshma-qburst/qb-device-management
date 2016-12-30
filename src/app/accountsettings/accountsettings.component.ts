1065a1167321cf113c3e9974b3979b99879d38ee
reshma <reshma@qburst.com>
2016-12-30 16:15:27 +0530

    account settings validation & changes

--- src/app/accountsettings/accountsettings.component.ts
+++ src/app/accountsettings/accountsettings.component.ts
@@ -16,29 +16,53 @@ export class AccountSettingsComponent implements OnInit {
 accountsettings: FormGroup;
 invalidLogin: boolean = false;
 returnUrl: string;
+invalidPassword: boolean = false;
 
-    constructor(
-		private route: ActivatedRoute,
-		private router: Router,
-		private formBuilder: FormBuilder,
-		private localstorage: LocalstorageService) {
-		this.accountsettings = new FormGroup({
-            oldpassword: new FormControl("", Validators.required),
-            newpassword: new FormControl("", Validators.required),
-            confirmpassword: new FormControl("", Validators.required)
-            });
-        this.accountsettings.valueChanges.subscribe(data => {
-            if (this.invalidLogin)
-                this.invalidLogin = false;
-        });
-    }
-    ngOnInit() {
-		this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/accountsettings";
-    }
+constructor(
+	private route: ActivatedRoute,
+	private router: Router,
+	private formBuilder: FormBuilder,
+	private localstorage: LocalstorageService) {
+	this.accountsettings = new FormGroup({
+	            oldpassword: new FormControl("", Validators.required),
+	            newpassword: new FormControl("", Validators.required),
+	            confirmpassword: new FormControl("", Validators.required)
+	            });
+	        this.accountsettings.valueChanges.subscribe(data => {
+	            if (this.invalidLogin)
+	                this.invalidLogin = false;
+	        });
+	    }
+	ngOnInit() {
+	this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/accountsettings";
+	}
 
 	changepassword(accountsettings) {
-		console.log(accountsettings);
-		accountsettings.submitted = true;
-		this.localstorage.getUser();
+		let value = accountsettings.value;
+		accountsettings.oldpasswordMismatch = false;
+		accountsettings.newpasswordSameAsOld = false;
+		accountsettings.newpasswordAndConfirmPassword = false;
+
+			if (value.oldpassword !== JSON.parse(this.localstorage.getUser()).password) {
+					accountsettings.oldpasswordMismatch = true;
+			} else if (value.newpassword === JSON.parse(this.localstorage.getUser()).password) {
+					accountsettings.newpasswordSameAsOld = true;
+			} else if (value.newpassword !== value.confirmpassword) {
+					accountsettings.newpasswordAndConfirmPassword = true;
+			}
+
+			accountsettings.submitted = true;
+			localStorage.setItem("newpassword", value.newpassword);
+			if (!accountsettings.oldpasswordMismatch &&
+				!accountsettings.newpasswordSameAsOld &&
+				!accountsettings.newpasswordAndConfirmPassword ) {
+				alert("Saved!");
+				this.clearFields(accountsettings);
+			}
+	}
+
+	clearFields(accountsettings) {
+		accountsettings.reset();
+		accountsettings.submitted = false; // remove form errors as well.
 	}
 }
